a29cdb7b2313691ea34f27baff9eec0ad9da6a44
Croud9 <61747240+Croud9@users.noreply.github.com>
2020-11-09 15:36:59 +0400

    Add files via upload

---
+++ Lesson3_2.0/station1.rb
@@ -0,0 +1,25 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @trains = []
+    @name = name
+  end
+
+  def add_train(train)
+    @trains << train
+  end
+
+  def trains_by_type(type)
+    @trains.each do |train|
+      if train.type == type
+        return [*train]
+      end
+    end
+  end
+
+  def delete_train(train)
+    @trains.delete(train)
+  end
+
+end
---
+++ Lesson3_2.0/train.rb
@@ -0,0 +1,70 @@
+class Train
+  attr_accessor :station
+  attr_accessor :speed
+  attr_reader :number
+  attr_reader :type
+  attr_reader :route
+  attr_accessor :count
+  attr_accessor :station
+
+  def initialize(number, type, count)
+    @speed = 0
+    @number = number
+    @type = type
+    @count = count
+  end
+
+  def up_speed(speed)
+    self.speed += speed
+  end
+
+  def brake
+    self.speed = 0
+  end
+
+  def carriage_add
+    if self.speed == 0
+      self.count += 1
+    else
+      puts 'Brake first'
+    end
+  end
+
+  def carriage_delete
+    if self.speed == 0
+      self.count -= 1
+    else
+      puts 'Brake first'
+    end
+  end
+
+  def route=(route)
+    @route = route
+    self.station = self.route.stations.first
+  end
+
+  def move_next_station
+    if station != route.stations.last
+      station.delete_train(self) 
+      self.station = self.next_station
+      station.add_train(self)
+    end
+  end
+
+  def move_previous_station
+    if station != route.stations.first
+      station.delete_train(self)
+      self.station = self.previous_station
+      station.add_train(self)
+    end
+  end
+
+  def previous_station
+    self.route.stations[self.route.stations.index(self.station) - 1]
+  end
+
+  def next_station
+    self.route.stations[self.route.stations.index(self.station) + 1]
+  end
+
+end
