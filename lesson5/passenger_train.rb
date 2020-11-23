3124835498fa76177fce3db20d0c46646667dd2f
Croud9 <61747240+Croud9@users.noreply.github.com>
2020-11-23 19:59:32 +0500

    Update passenger_train.rb

--- lesson5/passenger_train.rb
+++ lesson5/passenger_train.rb
@@ -1,9 +1,9 @@
-class PassengerTrain < Train
+class CargoTrain < Train
   attr_reader :type
 
   def initialize
-    @type = :passenger
-    register_instance
+    @type = :cargo
+    super(register_instance)
   end
 
 end
